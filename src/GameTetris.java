b17248bc45358130f35d7d38bda587bea76ecf8f
akloand <akloand@gmail.com>
2023-03-23 14:20:51 +0200

    111

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -1,15 +1,147 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.*;
 
 public class GameTetris {
+
+    final String TITLE_OF_PROGRAM = "Tetris";
+    final int BLOCK_SIZE = 25;
+    final int ARC_RADIUS = 6;
+    final int FIELD_WIDTH = 10; // in block
+    final int FIELD_HEIGHT = 18;
+    final int START_LOCATION = 180;
+    final int FIELD_DX = 7;
+    final int FIELD_DY = 26;
+    final int LEFT = 37;
+    final int UP = 38;
+    final int RIGHT = 39;
+    final int DOWN = 40;
+    final int SHOW_DELAY = 350; // delay for animation
+
+    final int[][][] SHAPES = {
+            {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {4, 0x00f0f0}}, // I
+            {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {4, 0xf0f0f0}}, // O
+            {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f0f0}}, // J
+            {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f0f0}}, // L
+            {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f0f0}}, // S
+            {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x00f0f0}}, // T
+            {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0xf00000}}, // Z
+    };
+
+    final int[] SCORES = {100, 300, 700, 1500};
+    int gameScores = 0;
+    int[][] mine = new int[FIELD_HEIGHT + 1][FIELD_WIDTH];
+    JFrame frame;
+    Canvas canvasPanel = new Canvas();
+    Random random = new Random();
+    Figure figure = new Figure();
+    boolean gameOver = false;
+
+    final int[][] GAME_OVER_MSG = {
+            {0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+            {1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+            {1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
+
+    };
+
+
     public static void main(String[] args) {
         new GameTetris().go();
 
     }
 
     void go() {
+        frame = new JFrame(TITLE_OF_PROGRAM);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setResizable(false);
+
+        canvasPanel.setBackground(Color.BLACK);
+
+        frame.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent event) {
+                if (!gameOver) {
+                    if (event.getKeyCode() == DOWN) figure.drop();
+                    if (event.getKeyCode() == UP) figure.rotate();
+                    if (event.getKeyCode() == LEFT || event.getKeyCode()== RIGHT) figure.move(event.getKeyCode());
+
+                }
+                canvasPanel.repaint();
+
+            }
+        });
+        frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        frame.setVisible(true);
+
+        Arrays.fill(mine[FIELD_HEIGHT], 1);
+
+        // main loop of game
+        while (!gameOver) {
+            try {
+                Thread.sleep(SHOW_DELAY);
+            } catch (Exception e) { e.printStackTrace();}
+            canvasPanel.repaint();
+            if (figure.isTouchGround()) {
+                figure.leaveOnTheGround();
+                checkFilling();
+                figure = new Figure();
+                gameOver = figure.isCrossGround();
+            } else {
+                figure.stepDown();
+            }
+        }
 
     }
+
+    void checkFilling() {
+
+    }
+
+    class Figure {
+
+        boolean isTouchGround() {
+            return false;
+        }
+
+        boolean isCrossGround() {
+            return false;
+        }
+
+        void leaveOnTheGround() {
+
+        }
+
+        void stepDown() {
+
+        }
+
+        void drop() {
+
+        }
+
+        void move(int direction) {
+
+        }
+
+        void rotate() {
+
+        }
+
+    }
+
+    class Block {
+    }
+
+    public class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics graphics) {
+            super.paint(graphics);
+
+        }
+    }
+
+
 }
