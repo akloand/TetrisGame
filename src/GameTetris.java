4cfdfdcb1b35adc85567c9d5c2afbaae3591ea76
akloand <akloand@gmail.com>
2023-03-23 11:06:50 +0200

    Initial commit

---
+++ src/GameTetris.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class GameTetris {
+    public static void main(String[] args) {
+        new GameTetris().go();
+
+    }
+
+    void go() {
+
+    }
+}
